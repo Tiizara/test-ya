f3c45c812a61c08bbc8cd5b8c90873c194e31796
Tiizara <135403209+Tiizara@users.noreply.github.com>
2024-02-06 13:57:14 +0500

    work

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,8 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
-import csso from 'postcss-csso';
-import rename from 'gulp-rename';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
-import htmlmin from 'gulp-htmlmin';
-import terser from 'gulp-terser';
-import squoosh from 'gulp-libsquoosh';
-import { deleteAsync } from 'del';
-import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
-
-
 
 // Styles
 
@@ -22,90 +12,18 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer(),
-      csso()
+      autoprefixer()
     ]))
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
+    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
-
-//HTML
-const html = () => {
-  return gulp.src('source/*.html')
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('build'));
-}
-
-// Scripts
-const script = () => {
-  return gulp.src('source/js/*.js')
-    .pipe(terser())
-    .pipe(gulp.dest('build/js'))
-}
-
-//Images
-const images = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
-    .pipe(squoosh())
-    .pipe(gulp.dest('build/img'))
-}
-
-const copyImages = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
-    .pipe(gulp.dest('build/img'))
-}
-
-//webp
-const createWebp = () => {
-  return gulp.src('source/img/**/*.{png,jpg}')
-    .pipe(squoosh({
-      webp: {}
-    }))
-    .pipe(gulp.dest('build/img'))
-}
-
-//SVG
-const svg = () =>
-  gulp.src(['source/img/**/*.svg', '!source/img/icons-sprite/*.svg'])
-    .pipe(svgo())
-    .pipe(gulp.dest('build/img'));
-
-const sprite = () => {
-  return gulp.src(['source/img/icons-sprite/*.svg'])
-    .pipe(svgo())
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'));
-}
-
-
-//Copy
-const copy = (done) => {
-  gulp.src([
-    'source/fonts/**/*.{woff2,woff}',
-    'source/*.ico',
-    'source/*.webmanifest'
-  ], {
-    base: 'source'
-  })
-    .pipe(gulp.dest('build'))
-  done();
-}
-
-//clean
-const clean = () => {
-  return deleteAsync('build')
-};
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'build'
+      baseDir: 'source'
     },
     cors: true,
     notify: false,
@@ -114,52 +32,14 @@ const server = (done) => {
   done();
 }
 
-// Reload
-
-const reload = (done) => {
-  browser.reload();
-  done();
-}
-
 // Watcher
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/js/**/*.js', gulp.series(script));
-  gulp.watch('source/*.html', gulp.series(html, reload));
+  gulp.watch('source/*.html').on('change', browser.reload);
 }
 
-// Build
-
-export const build = gulp.series(
-  clean,
-  copy,
-  images,
-  gulp.parallel(
-    styles,
-    html,
-    script,
-    svg,
-    sprite,
-    createWebp
-  ),
-);
-
-// Default
 
 export default gulp.series(
-  clean,
-  copy,
-  copyImages,
-  gulp.parallel(
-    styles,
-    html,
-    script,
-    svg,
-    sprite,
-    createWebp
-  ),
-  gulp.series(
-    server,
-    watcher
-  ));
+  styles, server, watcher
+);
--- source/js/script.js
+++ source/js/script.js
@@ -1,24 +1,37 @@
-const sliderControl = document.querySelector('.slider__control');
+// Получаем элементы слайдера
 const slider = document.querySelector('.slider');
-const mainNav = document.querySelector('.main-nav');
-const mainNavToggle = document.querySelector('.main-nav__toggle');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
 
-mainNav.classList.add('main-nav--closed');
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
 
-if (sliderControl) {
-  let flag = false;
-  sliderControl.addEventListener('click', () => {
-    if (flag) {
-      slider.classList.toggle('slider--after');
-      slider.classList.toggle('slider--before');
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
     } else {
-      slider.classList.toggle('slider--after');
-      flag = true;
+      slide.style.display = 'none';
     }
   });
 }
 
-mainNavToggle.addEventListener('click', () => {
-  mainNav.classList.toggle('main-nav--closed');
-  mainNav.classList.toggle('main-nav--opened');
-})
+// Инициализация слайдера
+updateSlider();
